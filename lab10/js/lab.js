c8a0d3c4d0b879dbb008bef1761246f6ef4ecb41
Squeezlebub <122490849+Squeezlebub@users.noreply.github.com>
2025-05-15 09:31:39 -0700

    lab 10 and minor homepage fixes

---
+++ lab10/js/lab.js
@@ -0,0 +1,33 @@
+//Function to generate random text of a fixed length
+function generateRandomText() {
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    const min = 3;
+    const max = 100;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Get a random starting index to slice the Lorem Ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // Generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
+  }
+
+//Add a listener for our button
+$("#make-convo").click(function(){
+    //Get our fake dialogue
+    const newText = generateRandomText();
+    //append our newly generated text to our output div
+    $("#output").append('<div class="text">' + newText + '</div>');
+});
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
