5676f7450972978aebe294bd5126d2381102f1a4
Squeezlebub <122490849+Squeezlebub@users.noreply.github.com>
2025-05-22 14:56:04 -0700

    lab12 stuff

---
+++ lab12/js/lab.js
@@ -0,0 +1,13 @@
+function whenClicked (){
+    let data=$("input").val();
+    if(data){
+        $("#output").append
+        console.log("There is some data");
+    }
+    
+    else {
+
+    }
+}
+
+$("#button").click(whenClicked);
