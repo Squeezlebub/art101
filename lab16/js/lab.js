5874a3603a4b3b91b06305d243dbb2d1db6ba85e
Squeezlebub <122490849+Squeezlebub@users.noreply.github.com>
2025-06-05 19:54:45 -0700

    Lab16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,16 +1,33 @@
 $("#submit").click(function(){
+// Using the core $.ajax() method
+let id = Math.ceil(Math.random()*1025);
+$.ajax({
+    // The URL for the request (from the api docs)
+    url: "https://pokeapi.co/api/v2/pokemon/"+id+"/",
+    // The data to send (will be converted to a query string)
+    data: { 
+            // here is where any data required by the api 
+            //   goes (check the api docs)
+          },
+    // Whether this is a POST or GET request
+    type: "GET",
+    // The type of data we expect back
+    dataType : "json",
+    // What do we do when the api call is successful
+    //   all the action goes in here
+    success: function(data) {
+        // do stuff
+        console.log(data);
+        $("#output").html(data.name+" #"+data.id);
+        $("#output").append("<br><img style='width:200px' src="+data.sprites.front_default+"></img>");
+        $("#output").append("<br>"+data.types[0].type.name+" ");
+        $("#output").append(data.types[1].type.name);
+    },
+    // What we do if the api call fails
+    error: function (jqXHR, textStatus, errorThrown) { 
+        // do stuff
+        console.log("Error:", textStatus, errorThrown);
+    }
+});
+});
     
-    $.ajax({
-        url: "https://xkcd.com/info.0.json",
-        data: {},
-        type: "GET",
-        dataType: "json",
-        success: function(data) {
-            console.log(data);
-            $("#output").html(data.month + data.year + data.title + "<br><img src=\""+data.img+"\"></img>");
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log("Error:", textStatus, errorThrown);
-        }
-    })
-})
